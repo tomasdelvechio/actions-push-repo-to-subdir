b9186eb4779e7334abdde746dc0f86e5936f6674
Tomas Delvechio <tdelvechio@unlu.edu.ar>
2020-09-18 22:23:56 -0300

    basic sync logic added

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,17 @@
-FROM alpine:3.10
+FROM ubuntu:latest
+
+LABEL repository="https://github.com/tomasdelvechio/actions-push-repo-to-subdir"
+LABEL homepage="https://github.com/tomasdelvechio/actions-push-repo-to-subdir"
+LABEL maintainer="Tomas Delvechio <tomasdelvechio17+github@gmail.com>"
+
+LABEL com.github.actions.name="GitHub Action to Sync Repo to Another Repo like subdirectory"
+LABEL com.github.actions.description="Automatically push repository content to another repo, but like sub dir in the target repository"
+#LABEL com.github.actions.icon="package"
+#LABEL com.github.actions.color="purple"
+
+RUN apt update && \
+    apt upgrade -y && \
+    apt install git 
+
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,30 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+#echo "Hello $1"
+#time=$(date)
+#echo "::set-output name=time::$time"
+
+set -e
+
+#FOLDER=$1
+#STARTER_NAME="${2:-name}"
+GITHUB_USERNAME=$1
+TARGET_REPOSITORY_PATH=$2
+DIRNAME="$(basename $(dirname $TARGET_REPOSITORY_PATH))"
+SUBDIR_NAME="${3:-$DIRNAME}"
+REPO_SOURCE_PATH=$(pwd)
+
+git config --global user.email "tomasdelvechio-actions-syn-repos@example.org"
+git config --global user.name "$GITHUB_USERNAME"
+
+#cd $BASE
+CLONE_DIR=/tmp/$TARGET_REPOSITORY_PATH
+mkdir -p $CLONE_DIR
+git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$TARGET_REPOSITORY_PATH.git $CLONE_DIR &> /dev/null
+
+cp -TR $REPO_SOURCE_PATH $TARGET_REPOSITORY_PATH/$SUBDIR_NAME
+cd $TARGET_REPOSITORY_PATH
+rm -rf $SUBDIR_NAME/.git $SUBDIR_NAME/.vscode
+git add
+git commit -m 'auto sync of repo with tomasdelvechio/actions-push-repo-to-subdir'
+git push
